8c20db761882efb0e009efb210d1bd2c8ba75ecd
Li184805054 <55072872+Li184805054@users.noreply.github.com>
2019-10-21 22:12:09 +0800

    Add files via upload

--- Example03.java
+++ Example03.java
@@ -1,16 +1,21 @@
-class Animal{
-    public Animal(String name){
-          System.out.println("我是一只"+name);
-    }
-}
-class Dog extends Animal{
-      public Dog(){
-             super("沙皮狗");
-      }
-}
-
 public class Example03{
-       public static void main(String[] args){
-           Dog dog = new Dog();
-       }
+     public static void main(String[] args){
+     Teacher te=new Teacher();
+     new Thread(te,"老师1").start();
+     new Thread(te,"老师2").start();
+     new Thread(te,"老师3").start();
 }
+}
+class Teacher implements Runnable{
+     private int notes=80;
+     public void run(){
+       while(true){
+          if(notes>0){
+          Thread th=Thread.currentThread();
+          String th_name=th.getName();
+        System.out.println(th_name+"正在分发第 "+notes--+" 份笔记");
+}
+}
+}
+}
+  
