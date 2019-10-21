42c91f622bc26c9c3f369b92bfb9a8b92c7ee838
Li184805054 <55072872+Li184805054@users.noreply.github.com>
2019-10-21 22:15:36 +0800

    Add files via upload

---
+++ Taxis.java
@@ -0,0 +1,23 @@
+public class Taxis{
+     public static void main(String[] args){
+     Taxi car=new Taxi();
+     new Thread(car,"出租车1").start();
+     new Thread(car,"出租车2").start();
+     new Thread(car,"出租车3").start();
+     new Thread(car,"出租车4").start();
+     new Thread(car,"出租车5").start();
+}
+}
+class Taxi implements Runnable{
+     private int passengers=100;
+     public void run(){
+       while(true){
+          if(passengers>0){
+          Thread th=Thread.currentThread();
+          String th_name=th.getName();
+        System.out.println(th_name+"正在接送第 "+passengers--+" 位乘客.");
+}
+}
+}
+}
+  
